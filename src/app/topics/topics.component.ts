9df47884418cb40267a3ba90357c5aa3f7acd70f
Vincent Sargeni <vsargeni@wifi-roaming-128-4-129-187.host.udel.edu>
2019-11-19 18:57:34 -0500

    latest

--- src/app/topics/topics.component.ts
+++ src/app/topics/topics.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import * as firebase from 'firebase/app';
-import { Observable } from 'rxjs';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import * as firebase from "firebase/app";
+import { Observable } from "rxjs";
+import { AngularFireDatabase, AngularFireList } from "@angular/fire/database";
 import {
   AngularFirestore,
   AngularFirestoreModule,
   AngularFirestoreDocument,
   fromDocRef
-} from '@angular/fire/firestore';
-import { Topic } from './topic.model';
+} from "@angular/fire/firestore";
+import { Topic } from "./topic.model";
 
 @Component({
-  selector: 'app-topics',
-  templateUrl: './topics.component.html',
-  styleUrls: ['./topics.component.css']
+  selector: "app-topics",
+  templateUrl: "./topics.component.html",
+  styleUrls: ["./topics.component.css"]
 })
-
 export class TopicsComponent implements OnInit {
   //itemsRef: AngularFireList<any>;
-  constructor(private route: ActivatedRoute, public db: AngularFirestore) { }
+  constructor(private route: ActivatedRoute, public db: AngularFirestore) {}
 
   //public viewTopics: Array<Topic>;
 
@@ -27,19 +26,35 @@ export class TopicsComponent implements OnInit {
     this.getTopics();
   }
 
-
   async getTopics() {
     // const firestore = firebase.firestore();
     const topics = [];
+    try {
+      //const snap = await this.db.collection("subreddits").get();
+      let snap = this.db.collection("root").doc("subreddits");
+      let getDoc = snap
+        .get().toPromise()
+        .then(doc => {
+          if (!doc.exists) {
+            console.log("No such document!");
+          } else {
+            console.log("Document data:", doc.data());
+          }
+        })
+        .catch(err => {
+          console.log("Error getting document", err);
+        });
 
-    // const snap = await this.db.collection('subreddits').doc('posts')
-    const snap = await this.db.collection('subreddits').doc('posts')
-    //const x = await this.afs.doc
+      //const x = await this.afs.doc
 
-    // snap.forEach(function (val) {
-    //   document.querySelector('#subs').innerHTML+=`<a href=${val}> ${val}</a><br>`;
-    // });
+      // snap.forEach(function(val) {
+         document.querySelector(
+           "#subs"
+         ).innerHTML += `<a href=${getDoc}> ${getDoc}</a><br>`;
+      // });
+    } catch (e) {
+      console.log(e);
+    }
     return topics;
   }
 }
-
