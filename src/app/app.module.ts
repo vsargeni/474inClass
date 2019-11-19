2ef7833d0332976c230b1cb6a7e821977b0a3e14
302Blue <abaldwin@udel.edu>
2019-11-19 15:08:16 -0500

    Changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Reactive Form
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Shared Components
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Angular & Firebase + Environment
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+//  Custom Components
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { TopicsComponent } from './topics/topics.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    TopicsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+  ],
+  bootstrap: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent
+  ]
+})
+export class AppModule { }
---
+++ src/app/topics/topics.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs';
+import {
+  AngularFirestore,
+  AngularFirestoreModule,
+  AngularFirestoreDocument,
+  fromDocRef
+} from '@angular/fire/firestore';
+import { Topic } from './topic.model';
+
+@Component({
+  selector: 'app-topics',
+  templateUrl: './topics.component.html',
+  styleUrls: ['./topics.component.css']
+})
+
+export class TopicsComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, public afs: AngularFirestore) { }
+
+  public viewTopics: Array<Topic>;
+
+  ngOnInit() {
+    this.getTopics();
+  }
+
+  async getTopics() {
+    // const firestore = firebase.firestore();
+    const topics = [];
+
+    const snap = await this.afs.collection('subreddits').get();
+
+    snap.forEach(val => {
+      document.querySelector('#subs').innerHTML += `<a href=${val}> ${val}</a><br>`;
+      topics.push(val);
+      console.log(val);
+    });
+
+    console.log(this.viewTopics);
+    return topics;
+  }
+}
+
