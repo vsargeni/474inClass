7622de3d9596024353d637e8758ac9e723e7d422
Korey Mitchell <koreymitchell@Koreys-MacBook-Pro.local>
2019-11-20 14:34:42 -0500

    updates

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../shared/services/auth.service';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.scss']
 })
 
 export class SignUpComponent implements OnInit {
